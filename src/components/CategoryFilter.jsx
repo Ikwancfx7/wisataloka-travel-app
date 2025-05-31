5fe0edd41204cb175fc99a9c48447faeac711d6b
Ikwanda <ikwandachairilf@gmail.com>
2025-05-31 22:17:56 +0700

    update style

--- src/components/CategoryFilter.jsx
+++ src/components/CategoryFilter.jsx
@@ -29,9 +29,9 @@ const CategoryFilter = ({ selectedCategory, onSelectCategory }) => {
         placeholder="Cari kategori..."
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
-        className="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+        className="w-1/2 md:w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
-      <div className="flex flex-wrap gap-2 justify-start">
+      <div className="flex flex-row md:flex-wrap gap-2 justify-start">
         <button
           onClick={() => onSelectCategory(null)}
           className={`px-3 py-1 rounded border ${
