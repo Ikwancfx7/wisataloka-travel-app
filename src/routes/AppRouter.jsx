15bc97731bf52403d2d6536272e6d14bc8075374
Ikwanda <ikwandachairilf@gmail.com>
2025-06-04 21:52:16 +0700

    menambahkan route admin

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -10,6 +10,11 @@ import AuthLayout from "../layout/AuthLayout";
 import Checkout from "../pages/Checkout";
 import TransactionDetile from "../pages/TransactionDetile";
 
+import AdminLayout from "../layout/AdminLayout";
+import AdminDashboard from "../pages/Admin/AdminDashboard";
+import ManageActivities from "../pages/Admin/ManageActivities";
+import ManageUsers from "../pages/Admin/ManageUsers";
+
 import { useAuth } from "../contexts/AuthContext";
 import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 
@@ -65,6 +70,33 @@ const AppRouter = () => {
             <TransactionDetile />
           </UserPrivateRoute>
         } />
+      </Route>
+
+      <Route path="/admin" element={<AdminLayout />}>
+        <Route
+          index
+          element={
+            <AdminRoute>
+              <AdminDashboard />
+            </AdminRoute>
+          }
+        />
+        <Route
+          path="activities"
+          element={
+            <AdminRoute>
+              <ManageActivities />
+            </AdminRoute>
+          }
+        />
+        <Route
+          path="users"
+          element={
+            <AdminRoute>
+              <ManageUsers />
+            </AdminRoute>
+          }
+        />
       </Route>
 
       <Route element={<AuthLayout />}>
