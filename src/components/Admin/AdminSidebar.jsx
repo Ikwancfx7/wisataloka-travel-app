e4ca186c348b7165f696dd0e77f7c0df0e174658
Ikwanda <ikwandachairilf@gmail.com>
2025-06-07 23:12:17 +0700

    menambah logout

--- src/components/Admin/AdminSidebar.jsx
+++ src/components/Admin/AdminSidebar.jsx
@@ -1,6 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext";
 
 const AdminSidebar = () => {
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    try {
+      logout();
+      navigate("/");
+    } catch (error) {
+      console.error("Logout gagal:", error);
+    }
+  }
+
   return (
     <aside className="w-64 bg-white shadow-md p-4 h-full">
       <h2 className="text-xl font-bold mb-4">Admin Panel</h2>
@@ -8,6 +21,7 @@ const AdminSidebar = () => {
         <Link to="/admin" className="hover:underline">Dashboard</Link>
         <Link to="/admin/activities" className="hover:underline">Manage Activities</Link>
         <Link to="/admin/users" className="hover:underline">Manage Users</Link>
+        <button onClick={handleLogout} className="hover:cursor-pointer bg-red-200 rounded-lg">Logout</button>
       </nav>
     </aside>
   );
