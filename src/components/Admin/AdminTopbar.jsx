a58ef5395a3255e885255d5c0eec578688496041
Ikwanda <ikwandachairilf@gmail.com>
2025-06-07 23:12:54 +0700

    menambah navProfile sebagai navbar untuk edit profile dan logout

--- src/components/Admin/AdminTopbar.jsx
+++ src/components/Admin/AdminTopbar.jsx
@@ -1,13 +1,15 @@
 import { useAuth } from "../../contexts/AuthContext"; 
+import NavbarProfile from "../NavProfile";
 
 const AdminTopbar = () => {
     const { user } = useAuth();
 
   return (
-    <header className="bg-white border-b px-6 py-4 shadow-sm">
+    <header className="flex flex-row justify-between bg-white border-b px-6 py-4 shadow-sm">
       <div className="text-lg font-semibold">
-        Welcome, {user?.name || "Admin"}
+        Welcome, {user.name}
       </div>
+      <NavbarProfile />
     </header>
   );
 };
