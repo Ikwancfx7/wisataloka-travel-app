73bdcada3f5a9b834ea0a4bd944fb00e0511abd0
Ikwanda <ikwandachairilf@gmail.com>
2025-06-10 22:18:10 +0700

    menambah bar untuk versi mobile

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,5 +1,7 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import FootbarMobile from "../components/Mobile/NavbarMobile";
+import TopbarMobile from "../components/Mobile/TopbarMobile";
 import { Outlet, useLocation } from "react-router-dom";
 
 const MainLayout = () => {
@@ -8,15 +10,21 @@ const MainLayout = () => {
 
   return (
     <div className="flex flex-col">
-      <div className={`w-full z-20 ${isLandingPage ? "absolute top-0 left-0" : "relative"}`}>
+      <div className={`hidden md:flex w-full z-20 ${isLandingPage ? "absolute top-0 left-0" : "relative"}`}>
         <Navbar isLandingPage={isLandingPage} />
+      </div>
+      <div className="md:hidden">
+        <TopbarMobile />
+        <FootbarMobile />
       </div>
       
       <main className="flex-grow">
         <Outlet />
       </main>
       
-      <Footer />
+      <div className="hidden md:flex">
+        <Footer />
+      </div>
     </div>
   );
 };
