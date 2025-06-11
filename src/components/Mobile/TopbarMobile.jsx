b59e16b4fd54871490376696cd18a082e50e8f51
Ikwanda <ikwandachairilf@gmail.com>
2025-06-11 23:27:31 +0700

    menambahkan icon cart

--- src/components/Mobile/TopbarMobile.jsx
+++ src/components/Mobile/TopbarMobile.jsx
@@ -1,7 +1,21 @@
+import { Link } from "react-router-dom";
+import { useCart } from "../../contexts/CartContext";
+
 const TopbarMobile = () => {
+    const { totalQuantity } = useCart();
     return (
-        <div className="flex flex-row justify-between items-center w-full px-5 md:px-13 lg:px-15 py-2">
-            <h1 className="text-2xl font-extralight">WISATALOKA</h1>
+        <div className="flex flex-row justify-between items-center w-full px-6 md:px-13 lg:px-15 pt-10 pb-2">
+            <div className="flex flex-row justify-between items-center w-full ">
+                <h1 className="text-2xl font-semibold">WISATALOKA</h1>
+            </div>
+            <Link to="/cart" className="relative flex flex-col items-center text-xs">
+                <img src="/icons/shopping-cart.png" alt="Cart" className="w-8 h-8" />
+                {totalQuantity > 0 && (
+                <span className="absolute top-0 right-0 text-[10px] bg-red-600 text-white rounded-full w-4 h-4 flex items-center justify-center">
+                    {totalQuantity}
+                </span>
+                )}
+            </Link>
         </div>
     )
 }
