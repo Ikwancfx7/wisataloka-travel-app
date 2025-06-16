842c4d72ae4ccf47d8194681b7ca3d23b7aea343
Ikwanda <ikwandachairilf@gmail.com>
2025-06-16 23:11:41 +0700

    update fetching API

--- src/api/PaymentApi.jsx
+++ src/api/PaymentApi.jsx
@@ -1,6 +1,6 @@
 import axiosInstance from "./AxiosInstance";
 
-export const fetchPaymentMethods = async () => {
+export const GetPaymentMethods = async () => {
     try {
         const response = await axiosInstance.get("/api/v1/payment-methods");
         return response.data.data;
@@ -9,7 +9,7 @@ export const fetchPaymentMethods = async () => {
     }
 };
 
-export const fetchMyTransactions = async () => {
+export const GetMyTransactions = async () => {
     try {
         const response = await axiosInstance.get("/api/v1/my-transactions");
         return response.data.data;
@@ -17,3 +17,57 @@ export const fetchMyTransactions = async () => {
         throw error.response?.data ?? { message: error.message || "Something went wrong" };
     }
 };
+
+export const GetTransactionById = async (id) => {
+    try {
+        const response = await axiosInstance.get(`/api/v1/transaction/${id}`);
+        return response.data.data;
+    } catch (error) {
+        throw error.response?.data ?? { message: error.message || "Something went wrong" };
+    }
+};
+
+export const GetAllTransactions = async () => {
+    try {
+        const response = await axiosInstance.get("/api/v1/all-transactions");
+        return response.data.data;
+    } catch (error) {
+        throw error.response?.data ?? { message: error.message || "Something went wrong" };
+    }
+};
+
+export const CreateTransaction = async (data) => {
+    try {
+        const response = await axiosInstance.post("/api/v1/create-transaction", data);
+        return response.data.data;
+    } catch (error) {
+        throw error.response?.data ?? { message: error.message || "Something went wrong" };
+    }
+};
+
+export const CancelTransaction = async (id) => {
+    try {
+        const response = await axiosInstance.post(`/api/v1/cancel-transaction/${id}`);
+        return response.data.data;
+    } catch (error) {
+        throw error.response?.data ?? { message: error.message || "Something went wrong" };
+    }
+};
+
+export const UpdateTransactionProofPayment = async (id, data) => {
+    try {
+        const response = await axiosInstance.post(`/api/v1/update-transaction-proof-payment/${id}`, data);
+        return response.data.data;
+    } catch (error) {
+        throw error.response?.data ?? { message: error.message || "Something went wrong" };
+    }
+}
+
+export const UpdateTransactionStatus = async (id, data) => {
+    try {
+        const response = await axiosInstance.post(`/api/v1/update-transaction-status/${id}`, data);
+        return response.data.data;
+    } catch (error) {
+        throw error.response?.data ?? { message: error.message || "Something went wrong" };
+    }
+}
