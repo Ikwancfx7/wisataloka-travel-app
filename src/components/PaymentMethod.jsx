cf67b34ff7b5bec840c24b13234a0263e91c575d
Ikwanda <ikwandachairilf@gmail.com>
2025-06-16 23:12:27 +0700

    rename fungsi

--- src/components/PaymentMethod.jsx
+++ src/components/PaymentMethod.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { fetchPaymentMethods } from "../api/PaymentApi";
+import { GetPaymentMethods } from "../api/PaymentApi";
 
 const PaymentMethod = ({ selectedPaymentMethod, onChange }) => {
     const [paymentMethods, setPaymentMethods] = useState([]);
@@ -8,7 +8,7 @@ const PaymentMethod = ({ selectedPaymentMethod, onChange }) => {
     useEffect(() => {
         const loadPaymentMethods = async () => {
             try {
-                const methods = await fetchPaymentMethods();
+                const methods = await GetPaymentMethods();
                 setPaymentMethods(methods);
             } catch (error) {
                 console.error("Error fetching payment methods:", error);
