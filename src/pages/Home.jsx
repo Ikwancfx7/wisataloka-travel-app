bed075d7c20b017226b2bb188d73117934d40c84
Ikwanda <ikwandachairilf@gmail.com>
2025-06-17 23:03:20 +0700

    import showbanners

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,21 @@
 import { Link } from "react-router-dom";
 import Promos from "../components/Promos";
 import CategoryFilterLanding from "../components/CategoryFilterLanding";
+import ShowBanners from "../components/ShowBanners";
 
 const HomePage = () => {
     const BGLanding = "/images/BGLanding.jpg";
     
     return (
         <>
-            <div className="relative flex items-center justify-center md:h-[560px]">
-                <img src={BGLanding} alt="BGLanding"
+            <div className="relative flex items-center justify-center md:h-[560px] mb-10">
+                {/* <img src={BGLanding} alt="BGLanding"
                     className="flex relative w-full h-full object-cover" 
-                />
-                <div className="container mx-auto absolute flex justify-center items-center">
-                    <div className="absolute flex flex-col gap-5 md:gap-6">
-                        <h1 className="text-white text-xl md:text-5xl">Welcome to Wisataloka</h1>
+                /> */}
+                <ShowBanners />
+                <div className="container mx-auto absolute flex justify-center items-center z-30">
+                    <div className="absolute flex flex-col items-center gap-5 md:gap-6">
+                        <h1 className="text-white text-xl md:text-5xl font-bold">Welcome to Wisataloka</h1>
                         <div className="flex justify-center">
                             <CategoryFilterLanding />
                         </div>
