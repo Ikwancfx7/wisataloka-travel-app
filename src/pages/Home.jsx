1e7ea9f6eb5d940e7e941682908c38270fcf8ae0
Ikwanda <ikwandachairilf@gmail.com>
2025-06-18 22:43:11 +0700

    update style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,16 +7,16 @@ const HomePage = () => {
     return (
         <div>
             
-            <div className="relative h-screen md:h-[560px] mb-10 bg-blue-400">
-                <div className="absolute inset-0 z-0">
+            <div className="relative w-full h-[360px] md:h-[560px] mb-10 bg-blue-400">
+                <div className="absolute inset-0 z-10">
                     <ShowBanners />
                 </div>
-                <div className="relative z-10 w-full h-full flex justify-center items-center">
+                <div className="relative w-full h-full flex justify-center items-center">
                     <div className="w-full max-w-screen-2xl mx-auto flex flex-col items-center gap-5 md:gap-6 px-4">
-                        <h1 className="text-white text-xl md:text-5xl font-bold text-center">
+                        <h1 className="text-white text-2xl md:text-5xl font-bold text-center z-10">
                             Welcome to Wisataloka
                         </h1>
-                        <div className="flex justify-center">
+                        <div className="flex justify-center z-10">
                             <CategoryFilterLanding />
                         </div>
                     </div>
