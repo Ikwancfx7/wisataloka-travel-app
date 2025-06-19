d55f581b648ccdae441ebc090c3f2c49d94b5acb
Ikwanda <ikwandachairilf@gmail.com>
2025-06-19 23:33:04 +0700

    menambahkan image

--- src/components/CategoryFilterLanding.jsx
+++ src/components/CategoryFilterLanding.jsx
@@ -57,15 +57,26 @@ const CategoryFilterLanding = () => {
         className="w-full px-4 py-2 rounded-3xl shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white text-center placeholder:text-center"
       />
       {showDropdown && (
-        <div className={`custom-scroll absolute z-10 mt-1 w-full bg-white border rounded-xl shadow-md max-h-60 overflow-auto transition-all duration-500 ease-in-out transform ${showDropdown ? "opacity-100 scale-100 translate-y-0 pointer-events-auto" : "opacity-0 scale-95 -translate-y-2 pointer-events-none"}`}>
+        <div className={`custom-scroll absolute z-10 mt-1 w-full bg-white border rounded-xl shadow-md max-h-60 p-2 overflow-auto transition-all duration-500 ease-in-out transform ${showDropdown ? "opacity-100 scale-100 translate-y-0 pointer-events-auto" : "opacity-0 scale-95 -translate-y-2 pointer-events-none"}`}>
           {filteredCategories.map((cat) => (
-            <button
-              key={cat.id}
-              onClick={() => handleCategorySelect(cat.id)}
-              className="w-full text-left px-4 py-2 hover:bg-blue-100 hover:cursor-pointer transition-colors duration-300 ease-in-out"
-            >
-              {cat.name}
-            </button>
+            <div className="flex flex-row items-center p-2 gap-1 hover:bg-green-100 rounded-lg">
+              <img 
+                src={cat.imageUrl} 
+                alt={`image ${cat.name}`} 
+                className="w-10 h-10 object-cover rounded-lg"
+                onError={(e) => {
+                  e.target.onerror = null; // cegah infinite loop
+                  e.target.src = "/images/default-activity.jpg"; // fallback jika gagal load dari API
+                }}
+              />
+              <button
+                key={cat.id}
+                onClick={() => handleCategorySelect(cat.id)}
+                className="w-full text-left px-4 py-2 hover:cursor-pointer transition-colors duration-300 ease-in-out"
+              >
+                {cat.name}
+              </button>
+            </div>
           ))}
         </div>
       )}
