9026aac18e085e7dc547b9cca42b5975f19e6692
Ikwanda <ikwandachairilf@gmail.com>
2025-06-25 23:16:54 +0700

    rename fungsi

--- src/api/BannerApi.jsx
+++ src/api/BannerApi.jsx
@@ -1,6 +1,6 @@
 import axiosinstance from "./AxiosInstance";
 
-export const GetBanners = async () => {
+export const getBanners = async () => {
     try {
         const response = await axiosinstance.get("/api/v1/banners");
         return response.data.data;
@@ -9,7 +9,7 @@ export const GetBanners = async () => {
     }
 };
 
-export const GetBannerById = async (id) => {
+export const getBannerById = async (id) => {
     try {
         const response = await axiosinstance.get(`/api/v1/banner/${id}`);
         return response.data.data;
@@ -18,7 +18,7 @@ export const GetBannerById = async (id) => {
     }
 };
 
-export const PostCreateBanner = async (createBannerData) => {
+export const postCreateBanner = async (createBannerData) => {
     try {
         const response = await axiosinstance.post("/api/v1/create-banner", createBannerData);
         return response.data;
@@ -27,7 +27,7 @@ export const PostCreateBanner = async (createBannerData) => {
     }
 };
 
-export const PostUpdateBanner = async (id, updateBannerData) => {
+export const postUpdateBanner = async (id, updateBannerData) => {
     try {
         const response = await axiosinstance.post(`/api/v1/update-banner/${id}`, updateBannerData);
         return response.data;
@@ -36,7 +36,7 @@ export const PostUpdateBanner = async (id, updateBannerData) => {
     }
 };
 
-export const DelDeleteBanner = async (id) => {
+export const delDeleteBanner = async (id) => {
     try {
         const response = await axiosinstance.delete(`/api/v1/delete-banner/${id}`);
         return response.data;
