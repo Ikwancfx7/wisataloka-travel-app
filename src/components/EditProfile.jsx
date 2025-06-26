e1fd7c174247486f717e655f6a88986a31af54e0
Ikwanda <ikwandachairilf@gmail.com>
2025-06-26 22:19:09 +0700

    import getloggedUser, postUpdateProfile, uploadImage

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -1,7 +1,7 @@
-import axiosInstance from "../api/AxiosInstance";
 import { useState, useEffect } from "react";
 import { toast } from "react-toastify";
-import { updateProfile } from "../api/ProfileApi";
+import { getloggedUser, postUpdateProfile } from "../api/ProfileApi";
+import { uploadImage } from "../api/UploadApi";
 import LogoutBtn from "./Logout";
 
 const EditProfile = () => {
@@ -17,8 +17,8 @@ const EditProfile = () => {
     useEffect(() => {
     const fetchProfile = async () => {
       try {
-        const res = await axiosInstance.get("/api/v1/user");
-        const user = res.data.data;
+        const res = await getloggedUser();
+        const user = res;
         setForm({
           name: user.name,
           email: user.email,
@@ -47,11 +47,10 @@ const EditProfile = () => {
 
         try {
             setLoading(true);
-            const res = await axiosInstance.post("/api/v1/upload-image", formData);
+            const res = await uploadImage(formData);
             setForm({
                 ...form, profilePictureUrl: res.data.url
             })
-            // toast("Image uploaded successfully!", { autoClose: 1000 });
         } catch (err) {
             console.error(err);
             toast("Failed to upload image.", { autoClose: 1000 });
@@ -63,7 +62,7 @@ const EditProfile = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await updateProfile(form);
+            const res = await postUpdateProfile(form);
             toast("Profile updated successfully!", { autoClose: 1000 });
             console.log(res);
             setTimeout(() => {
@@ -82,7 +81,7 @@ const EditProfile = () => {
                 profilePictureUrl: "", 
             };
 
-            await updateProfile(removePictureProfile);
+            await postUpdateProfile(removePictureProfile);
             
             setForm(removePictureProfile);
         } catch (error) {
@@ -90,46 +89,45 @@ const EditProfile = () => {
         }
     };
 
-
     return (
         <div>
             <div className="max-w-lg mx-auto p-5 bg-white">
-                <h2 className="text-2xl font-bold mb-4 px-5">Edit Profile</h2>
+                <h2 className="flex justify-center text-2xl font-bold mb-4 px-5">Edit Profile</h2>
                 <form onSubmit={handleSubmit} className="space-y-4 p-5">
                     <div>
-                    <label>Name</label>
-                    <input
-                        type="text"
-                        name="name"
-                        value={form.name}
-                        onChange={handleChange}
-                        className="w-full border p-2 rounded"
-                    />
-                    </div>
+                        <label>Name</label>
+                        <input
+                            type="text"
+                            name="name"
+                            value={form.name}
+                            onChange={handleChange}
+                            className="w-full border py-2 px-4 rounded-full"
+                        />
+                        </div>
                     <div>
-                    <label>Email</label>
-                    <input
-                        type="email"
-                        name="email"
-                        value={form.email}
-                        onChange={handleChange}
-                        className="w-full border p-2 rounded"
-                    />
-                    </div>
+                        <label>Email</label>
+                        <input
+                            type="email"
+                            name="email"
+                            value={form.email}
+                            onChange={handleChange}
+                            className="w-full border py-2 px-4 rounded-full"
+                        />
+                        </div>
                     <div>
-                    <label>Phone Number</label>
-                    <input
-                        type="text"
-                        name="phoneNumber"
-                        value={form.phoneNumber}
-                        onChange={handleChange}
-                        className="w-full border p-2 rounded"
-                    />
+                        <label>Phone Number</label>
+                        <input
+                            type="text"
+                            name="phoneNumber"
+                            value={form.phoneNumber}
+                            onChange={handleChange}
+                            className="w-full border py-2 px-4 rounded-full"
+                        />
                     </div>
                     <div>
                         <label className="block mb-1 font-medium">Upload Profile Picture</label>
                         <div className="flex items-center gap-4">
-                            <label className="cursor-pointer italic bg-blue-600 text-white px-4 py-2 rounded-xl hover:bg-blue-700 transition duration-200">
+                            <label className="cursor-pointer italic bg-blue-600 text-white px-3 py-2 rounded-xl hover:bg-blue-700 transition duration-200">
                                 Select Image
                                 <input
                                     type="file"
@@ -159,9 +157,9 @@ const EditProfile = () => {
                                 <button
                                     type="button"
                                     onClick={handleRemoveProfilePicture}
-                                    className="flex text-left px-4 py-2 text-sm italic rounded-2xl text-yellow-700 hover:text-yellow-600 transition duration-200 hover:cursor-pointer"
+                                    className="flex text-left px-3 py-1 text-sm italic rounded-xl border border-yellow-700 hover:bg-yellow-100 text-yellow-700 transition duration-200 hover:cursor-pointer"
                                     >
-                                    Delete Profile Picture
+                                    Delete
                                 </button>
                             </div>
         
@@ -176,12 +174,12 @@ const EditProfile = () => {
                                 <p className="text-sm text-gray-500">Profile picture not uploaded</p>
                             </div>
                         )}
-                    <div className="w-full flex justify-center">
+                    <div className="w-full flex justify-center pt-10">
                         <button
                             type="submit"
                             className="bg-blue-600 text-white px-4 py-2 rounded-xl hover:bg-blue-700 hover:cursor-pointer transition duration-200"
                             >
-                            Simpan Perubahan
+                            Save Changes
                         </button>
                     </div>
                 </form>
