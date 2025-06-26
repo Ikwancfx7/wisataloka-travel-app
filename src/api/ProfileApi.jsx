34d1414b872ba74456ad9a98ed0c6f4e1a9c120b
Ikwanda <ikwandachairilf@gmail.com>
2025-06-26 22:17:55 +0700

    menambah fungsi API

--- src/api/ProfileApi.jsx
+++ src/api/ProfileApi.jsx
@@ -1,6 +1,37 @@
 import axiosInstance from "./AxiosInstance";
 
-export const updateProfile = async (formData) => {
-  const res = await axiosInstance.post("/api/v1/update-profile", formData);
-  return res;
+export const getloggedUser = async () => {
+  try {
+    const response = await axiosInstance.get("/api/v1/user");
+    return response.data.data;
+  } catch (error) {
+    throw error.response?.data || error;
+  }
+};
+
+export const getAllUsers = async () => {
+  try {
+    const response = await axiosInstance.get("/api/v1/all-user");
+    return response.data.data;
+  } catch (error) {
+    throw error.response?.data || error;
+  }
+}
+
+export const postUpdateProfile = async (formData) => {
+  try {
+    const response = await axiosInstance.post("/api/v1/update-profile", formData);
+    return response.data;
+  } catch (error) {
+    throw error.response?.data || error;
+  }
+};
+
+export const postUpdateUserRole = async (id, data) => {
+  try {
+    const response = await axiosInstance.post(`/api/v1/update-user-role/${id}`, data);
+    return response.data;
+  } catch (error) {
+    throw error.response?.data || error;
+  }
 };
