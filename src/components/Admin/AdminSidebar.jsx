0c486f2bc8391fcfeca00818fe3227c223e98a5a
Ikwanda <ikwandachairilf@gmail.com>
2025-06-27 22:44:46 +0700

    menukar posisi nav

--- src/components/Admin/AdminSidebar.jsx
+++ src/components/Admin/AdminSidebar.jsx
@@ -23,8 +23,8 @@ const AdminSidebar = () => {
       <nav className="flex flex-col">
         <Link to="/admin" className={`px-2 py-2 transition-all duration-300 ease-in-out font-bold`}>Dashboard</Link>
         <Link to="/admin/users" className={`nav-link-admin ${isActive("/admin/users") ? "bg-blue-100" : ""}`}>Manage Users</Link>
-        <Link to="/admin/activities" className={`nav-link-admin ${isActive("/admin/activities") ? "bg-blue-100" : ""}`}>Manage Activities</Link>
         <Link to="/admin/transactions" className={`nav-link-admin ${isActive("/admin/transactions") ? "bg-blue-100" : ""}`}>Manage Transactions</Link>
+        <Link to="/admin/activities" className={`nav-link-admin ${isActive("/admin/activities") ? "bg-blue-100" : ""}`}>Manage Activities</Link>
         <Link to="/admin/promos" className={`nav-link-admin ${isActive("/admin/promos") ? "bg-blue-100" : ""}`}>Manage Promos</Link>
         <Link to="/admin/categories" className={`nav-link-admin ${isActive("/admin/categories") ? "bg-blue-100" : ""}`}>Manage Categories</Link>
         <Link to="/admin/banners" className={`nav-link-admin ${isActive("/admin/banners") ? "bg-blue-100" : ""}`}>Manage Banners</Link>
