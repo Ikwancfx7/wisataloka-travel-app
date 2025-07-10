f462a4b9750f5db9feab4ed372d410f1129a73cc
Ikwanda <ikwandachairilf@gmail.com>
2025-07-10 11:55:14 +0700

    add adminnavbar

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import AdminSidebar from "../components/Admin/AdminSidebar";
 import AdminTopbar from "../components/Admin/AdminTopbar";
+import AdminNavbarMobile from "../components/Admin/AdminNavbar";
 
 const AdminLayout = () => {
   return (
@@ -12,6 +13,9 @@ const AdminLayout = () => {
       <div className="flex-1 flex flex-col">
         {/* Topbar */}
         <AdminTopbar />
+
+        {/* Mobile Navigation */}
+        <AdminNavbarMobile />
 
         {/* Page Content */}
         <main className="flex-1 overflow-y-auto p-6">
